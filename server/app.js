7015c14507925b0c7518d305f85b26a3967797fb
shivamkumarjha18 <jhashivamkumar776@gmail.com>
2025-08-21 20:40:46 +0530

    new changes

--- server/app.js
+++ server/app.js
@@ -7,13 +7,24 @@ const categoryRoutes = require('./routes/categoryRoutes');
 const subcategoryRoutes = require('./routes/subcategoryRoutes');
 const productRoutes = require('./routes/productRoutes');
 
+
+const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Serve static files from React build
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// API routes
 app.use('/api/categories', categoryRoutes);
 app.use('/api/subcategories', subcategoryRoutes);
 app.use('/api/products', productRoutes);
+
+// Serve React for all other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
